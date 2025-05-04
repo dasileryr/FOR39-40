8b87c259d0b2cec2a8d5acc7aec47c52cade5b8b
Denis Zadvornyy <105483456+dasileryr@users.noreply.github.com>
2025-05-04 15:51:47 +0300

    update

---
+++ for39/for39/for39.cpp
@@ -0,0 +1,30 @@
+﻿//Даны целые положительные числа A и B (A < B). Вывести все целые числа от A до B включительно; при этом каждое число должно выводиться столько раз, каково его значение (например, число 3 выводится 3 раза).
+#include <iostream>
+#include <cmath> 
+#include <windows.h> 
+using namespace std;
+
+int main() {
+    SetConsoleOutputCP(1251);
+    SetConsoleCP(1251);      
+
+    int A, B;
+    cout << "Введите число A: ";
+    cin >> A;
+    cout << "Введите число B: ";
+    cin >> B;
+
+    if (A < B && A > 0) {
+        for (int i = A; i <= B; i++) {
+            for (int j = 0; j < i; j++) {
+                cout << i << " ";
+            }
+        }
+        cout << endl;
+    }
+    else {
+        cout << "Ошибка: A должно быть меньше B и оба числа должны быть положительными" << endl;
+    }
+
+    return 0;
+}
---
+++ for40/for40/for40.cpp
@@ -0,0 +1,19 @@
+﻿//Даны целые числа A и B (A < B). Вывести все целые числа от A до B
+//включительно; при этом число A должно выводиться 1 раз, число A + 1 должно выводиться 2 раза и т.д.
+
+#include <iostream>
+using namespace std;
+
+int main() {
+    int A, B;
+    cin >> A >> B;
+
+    for (int i = A; i <= B; ++i) {
+        for (int j = 0; j < i - A + 1; ++j) {
+            cout << i << " ";
+        }
+    }
+
+    cout << endl;
+    return 0;
+}
